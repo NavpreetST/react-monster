3c7171c560f81e4219805cac32c394a211887bf5
NavpreetST <999navtoor@gmail.com>
2025-07-20 21:17:57 +0200

    added some style!!!!

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,22 @@
-import { useState } from 'react'
-import UserList from './components/UserList'
-import Product from './components/Product'
-import Person from './components/Person'
-import Product2 from './components/Product2'
-import Card from './components/Card'
+import { useState } from "react"
 
 function App() {
 
   
-
+  const ValidPassword= ()=><h1>Valid Password</h1>
+  const InvalidPassword= ()=><h1>InValid Password</h1>
+  const [isValid, setIsValid] = useState(false)
+  const Password = () =>{
+    return isValid ? ValidPassword() : InvalidPassword()
+  }
   return (
     
-    // <div>
-    //   {/* <UserList/>,
-    //   <Product/>, */}
-    //   <Person name= "Navpreet" age= {19} />,
-    //   <Product2 name= "Banana" price= {200} />
-    //   <Card>
-    //     <h1>My card</h1>
-    //     <p>This is some content for card 1.</p>
-    //   </Card>
-    //   <Card>
-    //     <h1>My card</h1>
-    //     <p>This is some content for card 2.</p>
-    //   </Card>
-    //   <Card>
-    //     <h1>My card</h1>
-    //     <p>This is some content for card 3.</p>
-    //   </Card>
-
-    // </div>
+  
     <div>
-      
+      <button style = {{ color: "blue" }} onClick={()=> setIsValid(!isValid)}>On/Off</button>
+      <Password/>
+
+      <h1 style = {{ color: "blue", backgroundColor: "red", borderRadius: "200px"}}>Nothing to see here.</h1>
     </div>
     
   )
