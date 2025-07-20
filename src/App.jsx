0ab954be0c3debe9af104550d74d70bdd2a0f560
NavpreetST <999navtoor@gmail.com>
2025-07-20 19:51:38 +0200

    Part 1 in progress

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+import UserList from './components/UserList'
+import Product from './components/Product'
+import Person from './components/Person'
+import Product2 from './components/Product2'
+import Card from './components/Card'
+
+function App() {
+
+  
+
+  return (
+    
+    // <div>
+    //   {/* <UserList/>,
+    //   <Product/>, */}
+    //   <Person name= "Navpreet" age= {19} />,
+    //   <Product2 name= "Banana" price= {200} />
+    //   <Card>
+    //     <h1>My card</h1>
+    //     <p>This is some content for card 1.</p>
+    //   </Card>
+    //   <Card>
+    //     <h1>My card</h1>
+    //     <p>This is some content for card 2.</p>
+    //   </Card>
+    //   <Card>
+    //     <h1>My card</h1>
+    //     <p>This is some content for card 3.</p>
+    //   </Card>
+
+    // </div>
+    <div>
+      
+    </div>
+    
+  )
+}
+
+export default App
---
+++ src/components/Product2.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+const Product2 = ({name, price}) => {
+  return (
+    <div>
+        <h1>Name {name}</h1>
+        <h2>Price: ${price}</h2>
+    </div>
+  )
+}
+
+export default Product2
