d339c2d48c20dbc015cbfed1f3c9bea45014492d
NavpreetST <999navtoor@gmail.com>
2025-07-21 22:30:10 +0200

    Refined the template

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,3 @@
-import { useState } from 'react'
-import UserList from './components/UserList'
-import Product from './components/Product'
-import Person from './components/Person'
-import Product2 from './components/Product2'
-import Card from './components/Card'
 
 function App() {
 
@@ -11,25 +5,7 @@ function App() {
 
   return (
     
-    // <div>
-    //   {/* <UserList/>,
-    //   <Product/>, */}
-    //   <Person name= "Navpreet" age= {19} />,
-    //   <Product2 name= "Banana" price= {200} />
-    //   <Card>
-    //     <h1>My card</h1>
-    //     <p>This is some content for card 1.</p>
-    //   </Card>
-    //   <Card>
-    //     <h1>My card</h1>
-    //     <p>This is some content for card 2.</p>
-    //   </Card>
-    //   <Card>
-    //     <h1>My card</h1>
-    //     <p>This is some content for card 3.</p>
-    //   </Card>
-
-    // </div>
+   
     <div>
       
     </div>
